7786b84bd3bdd7afc7fa209bf35e9361b4549a37
Erwin <119857448+3-rwin@users.noreply.github.com>
2022-12-23 11:36:05 +0100

    Update Feed.js

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -10,7 +10,6 @@ const Feed = ({ buttons }) => {
         {buttons.map(button => (
           <Button key={button.id} button={button} />
         ))}
-        {console.log(settings)}
         {settings ? <NewButton /> : null}
     </div>
   )
